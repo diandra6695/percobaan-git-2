8d999352ed8e7a1f8f3ad6a8b8a06b61e62f2ac4
diandra6695 <andracom108@gmail.com>
2022-08-07 08:38:30 +0700

    memperbarui index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <title>mbajal</title>
+        meta
+    </head>
+    <body>
+        <h1>hello word</h1>
+        <p>lorem ipsum dolor sit amet conse</p>
+        <p>lorem ipsum dolor sit amettt</p>
+        
+    </body>
+</html>
